774db3296c6dcf5526a72a55b879b1f586eb925a
Chitra bisht <104213057+chitra36@users.noreply.github.com>
2023-03-24 17:46:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
 const promise1 = Promise.resolve("Hello, world!");
 
-promise1.then((value) => {
-  console.log(value);
+promise1.then((output) => {
+  console.log(output);
  
 });
 
