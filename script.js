27c8c98905259a4410f1247c7f56b773c6924d16
Chitra bisht <104213057+chitra36@users.noreply.github.com>
2023-03-24 18:12:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-let output = new Promise(function(resolve, reject) {
- 
+function myDisplayer(some) {
+  document.getElementById("output").innerHTML = some;
+}
+
+let myPromise = new Promise(function(myResolve, myReject) {
+  let x = 0;
+
+// some code (try to change x to 5)
+
+  if (x == 0) {
+    myResolve("Hello, world!");
+  } else {
+    myReject("Error");
+  }
 });
+
+myPromise.then(
+  function(value) {myDisplayer(value);},
+  function(error) {myDisplayer(error);}
+);
