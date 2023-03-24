6de9a3ca07cbcfb4bac899f576b15de4b8d0e3e9
Chitra bisht <104213057+chitra36@users.noreply.github.com>
2023-03-24 18:31:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function myDisplayer(some) {
 let myPromise = new Promise(function(myResolve, myReject) {
   let x = 0;
 
-// some code (try to change x to 5)
+
 
   if (x == 0) {
     myResolve("Hello, world!");
